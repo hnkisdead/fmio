9de9156193a040935ba427e4816285ba8ef3e20d
Vasily Subbotin <subv13@yandex.ru>
2023-04-05 16:09:52 +0500

    remove color hardcode and add responsivnes

--- src/ResultsSummary/index.tsx
+++ src/ResultsSummary/index.tsx
@@ -6,8 +6,8 @@ import { ReactComponent as VisualIcon } from './assets/icon-visual.svg'
 
 function Score({ score, percentage }: { score: number; percentage: number }): JSX.Element {
     return (
-        <div className="flex min-h-[450px] basis-1/2 flex-col items-center justify-between rounded-3xl bg-gradient-to-b from-[#7857FF] to-[#2E2BE9] pb-[50px] pt-[40px]">
-            <div className="text-lg font-bold text-[#C8C7FF]">Your result</div>
+        <div className="flex min-h-[450px] basis-1/2 flex-col items-center justify-between rounded-b-3xl bg-gradient-to-b from-[#7857FF] to-[#2E2BE9] pb-12 pt-10 sm:rounded-3xl">
+            <div className="text-lg font-bold text-violet-300">Your result</div>
 
             <div className="flex min-h-[170px] min-w-[170px] flex-col items-center justify-center rounded-full bg-gradient-to-b from-[#4E21CA] to-transparent">
                 <div className="mb-2 text-6xl font-bold text-white">{score}</div>
@@ -16,7 +16,7 @@ function Score({ score, percentage }: { score: number; percentage: number }): JS
 
             <div className="text-2xl font-bold text-white">Great</div>
 
-            <div className="max-w-[240px] text-center text-[#C8C7FF]">
+            <div className="w-3/4 text-center text-violet-300">
                 You scored higher then {percentage}% of the people who have taken these tests
             </div>
         </div>
@@ -37,11 +37,9 @@ function SummaryItem({
     bgCls: string
 }): JSX.Element {
     return (
-        <div
-            className={`flex min-h-[50px] min-w-[250px] flex-row items-center justify-between rounded-lg px-[15px] font-bold ${bgCls}`}
-        >
+        <div className={`flex h-12 w-3/4 flex-row items-center justify-between rounded-lg px-4 font-bold ${bgCls}`}>
             <div className="flex flex-row">
-                <span className="mr-[15px]">{renderIcon()}</span>
+                <span className="mr-3">{renderIcon()}</span>
                 <span className={textCls}>{name}</span>
             </div>
             <div className="text-sm">
@@ -55,42 +53,42 @@ function SummaryItem({
 
 function Summary({ summary }: { summary: Record<string, number> }): JSX.Element {
     return (
-        <div className="flex min-h-[450px] basis-1/2 flex-col items-center justify-between rounded-3xl pb-[50px] pt-[40px]">
-            <div className="mb-4 min-w-[250px] text-left text-lg font-bold">Summary</div>
+        <div className="flex min-h-[450px] basis-1/2 flex-col items-center justify-between rounded-3xl pb-12 pt-10">
+            <div className="mb-4 w-3/4 text-left text-lg font-bold">Summary</div>
 
             <SummaryItem
                 name="Reaction"
                 value={summary.reaction}
-                renderIcon={() => <ReactionIcon />}
-                textCls="text-[#FF5757]"
-                bgCls="bg-[#FFF6F5]"
+                renderIcon={() => <ReactionIcon className="stroke-red-500" />}
+                textCls="text-red-500"
+                bgCls="bg-red-50"
             />
 
             <SummaryItem
                 name="Memory"
                 value={summary.memory}
-                renderIcon={() => <MemoryIcon />}
-                textCls="text-[#FFB01F]"
-                bgCls="bg-[#FFFBF2]"
+                renderIcon={() => <MemoryIcon className="stroke-yellow-500" />}
+                textCls="text-yellow-500"
+                bgCls="bg-yellow-50"
             />
 
             <SummaryItem
                 name="Verbal"
                 value={summary.verbal}
-                renderIcon={() => <VerbalIcon />}
-                textCls="text-[#00BD91]"
-                bgCls="bg-[#F2FBFA]"
+                renderIcon={() => <VerbalIcon className="stroke-green-500" />}
+                textCls="text-green-500"
+                bgCls="bg-green-50"
             />
 
             <SummaryItem
                 name="Visual"
                 value={summary.visual}
-                renderIcon={() => <VisualIcon />}
-                textCls="text-[#1125D4]"
-                bgCls="bg-[#F3F3FD]"
+                renderIcon={() => <VisualIcon className="stroke-blue-500" />}
+                textCls="text-blue-500"
+                bgCls="bg-blue-50"
             />
 
-            <button className="mt-4 min-h-[50px] min-w-[250px] rounded-full bg-[#303b59] text-white">Continue</button>
+            <button className="mt-4 h-12 w-3/4 rounded-full bg-slate-700 text-white">Continue</button>
         </div>
     )
 }
@@ -108,7 +106,7 @@ export function ResultsSummary(): JSX.Element {
 
     return (
         <div className="container mx-auto flex min-h-screen items-center justify-center">
-            <div className="flex min-h-[450px] min-w-[640px] items-center justify-center rounded-3xl bg-white drop-shadow-2xl">
+            <div className="min-w-full items-center justify-center bg-white drop-shadow-2xl sm:flex sm:min-h-[450px] sm:min-w-[640px] sm:rounded-3xl">
                 <Score score={score} percentage={percentage} />
                 <Summary summary={summary} />
             </div>
